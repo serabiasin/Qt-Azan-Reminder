1b4d9cd40ea1f27be238da75dc0d4cc5b6e1af8e
serabiasin <ahmadalfiazdzikri@gmail.com>
2017-08-15 17:06:19 +0700

    added view table for schedule

--- interface_jadwal/jadwal_azan.cpp
+++ interface_jadwal/jadwal_azan.cpp
@@ -2,8 +2,30 @@
 
 jadwal_azan::jadwal_azan(QWidget *parent) : QWidget(parent)
 {
-QLabel *testing=new QLabel("Jadwal Azan");
-layout=new QVBoxLayout;
-layout->addWidget(testing);
-setLayout(layout);
+
+    layout_rapi=new QGroupBox(tr("Jadwal Azan Hari ini :"));
+    view=new QVBoxLayout;
+
+
+    this->setting_up();
+}
+
+
+void jadwal_azan::setting_up(){
+
+QTableWidget *tabel=new QTableWidget;
+tabel->setColumnCount(2);
+tabel->setRowCount(4);
+
+QStringList header;
+header<<"Sholat"<<"Waktu";
+tabel->setHorizontalHeaderLabels(header);
+//tabel->setItem(0,0,QTableWidgetItem("Subuh"));
+
+view->addWidget(tabel);
+setLayout(view);
+
+
+
+
 }
--- interface_jadwal/jadwal_azan.h
+++ interface_jadwal/jadwal_azan.h
@@ -3,20 +3,28 @@
 
 #include <QWidget>
 #include <QLabel>
+#include <QGroupBox>
+#include <QTableWidget>
+#include <QGridLayout>
 #include <QVBoxLayout>
+#include <QString>
 
 class jadwal_azan : public QWidget
 {
     Q_OBJECT
 public:
     explicit jadwal_azan(QWidget *parent = nullptr);
-
-signals:
-
-public slots:
-
+    void set_azan_time();
+    void setting_up();
 private:
-    QVBoxLayout *layout;
+    QLabel *shubuh_time;
+    QLabel *dzuhur_time;
+    QLabel *ashar_time;
+    QLabel *maghrib_time;
+    QLabel *isha_time;
+    QGroupBox *layout_rapi;
+    QVBoxLayout *view;
+
 };
 
 #endif // JADWAL_AZAN_H
